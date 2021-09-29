87342e5a50d10b2f11fb5a157db6565321482583
DedeSnrwn13 <sunarwandede13@gmail.com>
2021-09-29 09:44:36 +0700

    Integer Float

--- index.php
+++ index.php
@@ -1,20 +1,10 @@
 <?php 
 
-// $kota = ucwords("jakarta"); // untuk membuat huruf prtama pada setiap kata jadi Kapital
-// $kota = strtolower("BANDUNG"); // untuk membuat huruf menjadi kecil semua
-// $kota = strtoupper("semarang"); // untuk membuat huruf menjadi Kapital semua
+$a = 10;
+$b = 3;
+$c = $a / $b;
 
+// echo $c;
+echo round($c);
+// fungsi round ototmatis membulatkan jika lebih dari 0,5
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Selamat Datang di, <?php echo $kota; ?></h1>
-</body>
-</html>
