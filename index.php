3f8fddb2a7a773edfc19bdd1f4ca59d14f4ba4ac
DedeSnrwn13 <sunarwandede13@gmail.com>
2021-09-29 11:30:09 +0700

    Array Asosiatif

--- index.php
+++ index.php
@@ -1,8 +1,19 @@
 <?php 
 
-$a = array("Ayam", "Kuda", "Buaya");
-// Menambah data kedalam Array
-$a[] = "Babi";
-print_r($a);
+// Cara 1
+// $datadiri["nama"] = "Robi";
+// $datadiri["goldarah"] = "A";
+// $datadiri["alamat"] = "Batam";
+
+// Cara 2
+$datadiri = [
+    "nama" => "Budi",
+    "goldarah" => "O",
+    "alamat" => "Jakarta"
+];
+
+echo "Nama: " . $datadiri["nama"] . "<br>";
+echo "Gol Darah: " . $datadiri["goldarah"] . "<br>";
+echo "Alamat: " . $datadiri["alamat"] . "<br>";
 
 ?>
