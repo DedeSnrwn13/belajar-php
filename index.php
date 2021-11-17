f32654fd58faaede2d3ed8de35a158bc84f05cfd
DedeSnrwn13 <sunarwandede13@gmail.com>
2021-11-17 12:17:40 +0700

    Query String dengan GET

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
 <?php
-    $nama   = "Toni Haryanto";
+    $nama   = $_GET['nama'];
+    $alamat = $_GET['alamat'];
 ?>
 <!DOCTYPE html>
 <html>
 <body>
+    <form action="">
+        <input type="text" name="nama">
+        <input type="text" name="alamat">
+        <input type="submit">
+    </form>
     <h1>Selamat Datang <?php echo $nama; ?></h1>
+    <h1>Alamat <?php echo $alamat; ?></h1>
     <p>Selamat belajar pemrograman PHP</p>
 </body>
 </html>
