dc57239df0e1a1ace31dc3876c23733c0b0f2f5b
DedeSnrwn13 <sunarwandede13@gmail.com>
2021-11-20 14:46:05 +0700

    While

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <?php
-   $warna  = [
-                'merah','biru','kuning','merah','hijau','orange','ungu',
-                'biru','kuning','merah','hijau','orange', 'ungu', 'merah'
-            ];
+   $warna  = ['merah','biru','kuning','merah','hijau','orange','ungu',
+            'biru','kuning','merah','hijau','orange', 'ungu', 'merah'];
     
     $jumlah_merah = 0;
 
-    for ($i = 0; $i < count($warna); $i++) { 
+    $i = 0;
+    while ($i < 15) {
         if ($warna[$i] == 'merah') {
             $jumlah_merah++;
         }
-    }
+        $i++;
+    }   
 
     echo "Jumlah warna merah pada array sebanyak $jumlah_merah";
 ?>
