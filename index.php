d86eaaaecd3c96391ad001206e05331d2d4da5d3
DedeSnrwn13 <sunarwandede13@gmail.com>
2021-11-21 14:59:27 +0700

    Membuat Fungsi Sendiri

--- index.php
+++ index.php
@@ -1,38 +1,11 @@
 <?php
-   $siswa = [
-            ['nama' => 'Riki', 'kelas' => '2A', 'goldarah' => 'A'],
-            ['nama' => 'Azli', 'kelas' => '2B', 'goldarah' => 'AB'],
-            ['nama' => 'Benar', 'kelas' => '2C', 'goldarah' => 'O'],
-            ['nama' => 'Sabar', 'kelas' => '2D', 'goldarah' => 'B'],
-       ];
-?>
+   function cetakPesan($nama = null) {
+       if (empty($nama)) {
+           echo "Selamat Datang!";
+       } else {
+           echo "Selamat datang, $nama!";
+       }
+   }
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <table border="1">
-        <thead>
-            <tr>
-                <th>Nama</th>
-                <th>Kelas</th>
-                <th>Gol. Darah</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($siswa as $key => $data): ?>
-                <tr>
-                    <td><?php echo $data['nama']; ?></td>
-                    <td><?php echo $data['kelas']; ?></td>
-                    <td><?php echo $data['goldarah']; ?></td>
-                </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
-</body>
-</html>
+   cetakPesan('rizal');
+?>
