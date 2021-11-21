a6207b447788c53ca12fdd16deed4b3033eafee1
DedeSnrwn13 <sunarwandede13@gmail.com>
2021-11-21 14:26:34 +0700

    Menampilkan Data ke Table

--- index.php
+++ index.php
@@ -1,23 +1,38 @@
 <?php
    $siswa = [
-            ['nama' => 'Riki', 'kelas' => '2A'],
-            ['nama' => 'Azli', 'kelas' => '2B'],
-            ['nama' => 'Benar', 'kelas' => '2C'],
-            ['nama' => 'Sabar', 'kelas' => '2D'],
+            ['nama' => 'Riki', 'kelas' => '2A', 'goldarah' => 'A'],
+            ['nama' => 'Azli', 'kelas' => '2B', 'goldarah' => 'AB'],
+            ['nama' => 'Benar', 'kelas' => '2C', 'goldarah' => 'O'],
+            ['nama' => 'Sabar', 'kelas' => '2D', 'goldarah' => 'B'],
        ];
-    
-
-    foreach ($siswa as $key => $value) {
-        if ($value['kelas'] == '2A') 
-            continue;
-
-        echo "Nama: ". $value['nama'] . "<br>";
-        echo "Kelas: ". $value['kelas'] . "<br><br>";
-        
-
-        // if ($value['kelas'] != '2A') {
-        //     echo "Nama: ". $value['nama'] . "<br>";
-        //     echo "Kelas: ". $value['kelas'] . "<br><br>";
-        // }
-    } 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table border="1">
+        <thead>
+            <tr>
+                <th>Nama</th>
+                <th>Kelas</th>
+                <th>Gol. Darah</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($siswa as $key => $data): ?>
+                <tr>
+                    <td><?php echo $data['nama']; ?></td>
+                    <td><?php echo $data['kelas']; ?></td>
+                    <td><?php echo $data['goldarah']; ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+</body>
+</html>
