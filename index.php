3c9b7f332a491ab13e6f174d04f80eda757aeeee
DedeSnrwn13 <sunarwandede13@gmail.com>
2021-11-25 10:15:55 +0700

    Menulis Dan Membaca File

--- index.php
+++ index.php
@@ -1,17 +1,10 @@
 <?php
 
-function kaliLiapat(&$angka)
-{
-    $angka = $angka * 2;
-}
+$message = "Selamat Datang, Apa kabar?\n"; // \n untuk membuat baris baru
+// menulis file
+file_put_contents('content.txt', $message, FILE_APPEND);
+// membaca file 
+$isi_file = file_get_contents('content.txt');
+echo $isi_file;
 
-$x = 20;
-echo $x .'<br>';
-kaliLiapat($x);
-echo $x .'<br>';
-kaliLiapat($x);
-echo $x .'<br>';
-kaliLiapat($x);
-echo $x .'<br>';
-kaliLiapat($x);
 ?>
