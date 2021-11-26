d966e70f21a4d1e41f76b240ed415457ba3c1580
DedeSnrwn13 <sunarwandede13@gmail.com>
2021-11-26 12:39:06 +0700

    Membuat Aplikasi Todo Menghapus Data

--- index.php
+++ index.php
@@ -1,24 +1,24 @@
 <?php
     //total array yang disiapkan untuk disimpan
     $todos  = []; 
-    
+
     //melakukan pengecekan apakah file todo.txt ditemukan
     if(file_exists('todo.txt')) {
         //membaca file todo.txt
-        $file   =   file_get_contents('todo.txt');
+        $file = file_get_contents('todo.txt');
         //mengubah format serialize menjadi array
-        $todos  =   unserialize($file);
+        $todos = unserialize($file);
     }
 
     //Jika ditemukan todo yang dikirim melalui methode POST
     if(isset($_POST['todo'])) {
         $data   = $_POST['todo']; // data yang dipilih pada form
-        $todos[]=[
-                    'todo'  => $data,
-                    'status'=>0
+        $todos[] = [
+                    'todo'=> $data,
+                    'status' => 0
                 ];
-        $daftar_belanja=serialize($todos);
-        file_put_contents('todo.txt',$daftar_belanja);
+        $daftar_belanja = serialize($todos);
+        file_put_contents('todo.txt', $daftar_belanja);
         //redirect halaman
         header('location:index.php');
     }
@@ -26,9 +26,17 @@
     //jika ditemukan $_GET['status']
     if(isset($_GET['status'])) {
         //ubah status
-        $todos[$_GET['key']]['status']=$_GET['status'];
-        $daftar_belanja=serialize($todos);
-        file_put_contents('todo.txt',$daftar_belanja);
+        $todos[$_GET['key']]['status'] = $_GET['status'];
+        $daftar_belanja = serialize($todos);
+        file_put_contents('todo.txt', $daftar_belanja);
+        //redirect halaman
+        header('location:index.php');
+    }
+
+    // mengapus todo
+    if (isset($_GET['hapus'])) {
+        unset($todos[$_GET['key']]);
+        file_put_contents('todo.txt', $daftar_belanja);
         //redirect halaman
         header('location:index.php');
     }
@@ -43,21 +51,23 @@
 </form>
 <ul>
     <ul>
-    <?php foreach($todos as $key=>$value): ?>
+    <?php foreach($todos as $key => $value): ?>
     <li>
-        <input type="checkbox" name="todo" onclick="window.location.href='index.php?status=<?php echo($value['status']==1)? '0': '1'; ?>&key=<?php echo $key;?>'";
-        <?php if($value['status']==1)echo 'checked' ?>>
+        <input type="checkbox" name="todo" 
+            onclick="window.location.href='index.php?status=<?php echo($value['status']==1)? '0': '1'; ?>&key=<?php echo $key;?>'";
+            <?php if($value['status'] == 1) echo 'checked'; ?>
+        >
         <label>
             <?php
-                if($value['status']==1) {
-                echo '<del>'.$value['todo'].'</del>'; 
+                if($value['status'] == 1) {
+                    echo '<del>'.$value['todo'].'</del>'; 
                 }
                 else {
                     echo $value['todo'];
                 }
             ?>
         </label>
-        <a href='#'>hapus</a>
+        <a href="index.php?hapus=1&key=<?php echo $key; ?>">Hapus</a>
     </li>
     <?php endforeach; ?>
 </ul>
